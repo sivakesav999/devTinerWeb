a195ee6785138cd8bcaf52ce25b920110421f386
sivakesav999 <sivakesav999@gmail.com>
2026-09-07 18:33:42 +0530

    bug : navigation to feed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -33,7 +33,7 @@ const NavBar = () => {
     <>
       <div className="navbar bg-base-200 shadow-sm">
         <div className="flex-1">
-          <Link to="/" className="btn btn-ghost text-xl">
+          <Link to="/feed" className="btn btn-ghost text-xl">
             DevTinder
           </Link>
         </div>
